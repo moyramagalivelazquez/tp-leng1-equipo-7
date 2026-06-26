91a1415729e0bf3ccb3c27db23b9e0bb839c1695
moyramagalivelazquez <moyma12345@gmail.com>
2026-06-26 04:49:34 +0000

    js agregado y modificado

--- tp-leng1-equipo-7/JS/script.js
+++ tp-leng1-equipo-7/JS/script.js
@@ -1,9 +1,39 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    const contadores = document.querySelectorAll(".display-6, .display-5");
+
+    contadores.forEach(contador => {
+
+        const objetivo = Number(contador.textContent.replace("+", ""));
+        let numero = 0;
+
+        const velocidad = objetivo / 50;
+
+        const intervalo = setInterval(() => {
+
+            numero += velocidad;
+
+            if (numero >= objetivo) {
+                contador.textContent = "+" + objetivo;
+                clearInterval(intervalo);
+            } else {
+                contador.textContent = "+" + Math.floor(numero);
+            }
+
+        }, 30);
+
+    });
+
+});
+
 const formulario = document.querySelector("form");
 
-formulario.addEventListener("submit", function(e){
-    e.preventDefault();
+if (formulario) {
+    formulario.addEventListener("submit", function(e){
+        e.preventDefault();
 
-    alert("¡Gracias por querer formar parte de Red Rodolfo al Rescate! 🐶❤️");
+        alert("¡Gracias por querer formar parte de Red Rodolfo al Rescate! 🐶❤️");
 
-    formulario.reset();
-});
+        formulario.reset();
+    });
+}
